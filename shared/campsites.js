2e5d6d778c30de98e5e0dbe5c89984d8cacda665
Albert Garcia <albert.garcia1324@gmail.com>
2022-04-20 09:16:00 -0400

    Setup

---
+++ shared/campsites.js
@@ -0,0 +1,36 @@
+export const CAMPSITES = [
+  {
+    id: 0,
+    name: "Maryland",
+    image: "/images/maryland.png",
+    elevation: 1233,
+    featured: false,
+    description: "Contains a list of the most popular gyms located in Maryland",
+  },
+  {
+    id: 1,
+    name: "District of Columbia ",
+    image: "/images/chrome-river.jpg",
+    elevation: 877,
+    featured: false,
+    description: "Contains a list of the most popular gyms located in D.C.",
+  },
+  {
+    id: 2,
+    name: "Northern Virginia",
+    image: "/images/breadcrumb-trail.jpg",
+    elevation: 2901,
+    featured: false,
+    description:
+      "Contains a list of the most popular gyms located in Northern Virginia",
+  },
+  // {
+  //   id: 3,
+  //   name: "Redux Woods Campground",
+  //   image: "/images/redux-woods.jpg",
+  //   elevation: 42,
+  //   featured: true,
+  //   description:
+  //     "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  // },
+];
